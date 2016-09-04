bfc952e315b3b4101f7e83bf29a450dc9abf2190
gskim.korts.co.kr <jlife0113@gmail.com>
2016-09-05 02:29:44 +0900

    imported styles and scripts(jQuery and bootstrap), bundle

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/Styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jqurey-3.1.0.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jqurey-3.1.0.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+        }
+    }
+}
--- SimpleBlog/Global.asax.cs
+++ SimpleBlog/Global.asax.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace SimpleBlog
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);           
         }
     }
 }
