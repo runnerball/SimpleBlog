429f9f185b805f4d7bd3a422ff2574d0b6bb2e9c
gskim.korts.co.kr <jlife0113@gmail.com>
2016-09-13 06:17:48 +0900

    fluent migration

---
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsString(256)
+                .WithColumn("password_hash").AsString(128);
+
+            //throw new NotImplementedException();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("users");
+            //throw new NotImplementedException();
+        }
+        
+    }
+}
