2972d5941303f8bdaf5839b4254661b251068435
gskim.korts.co.kr <jlife0113@gmail.com>
2016-10-10 23:19:06 +0900

    bycrpt

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -5,11 +5,19 @@ using System.Web;
 using System.Web.Mvc;
 using SimpleBlog.ViewModels;
 using System.Web.Security;
+using NHibernate.Linq;
+using SimpleBlog.Models;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToRoute("home");
+        }
+
         // GET: Auth
         public ActionResult Login()
         {
@@ -18,18 +26,20 @@ namespace SimpleBlog.Controllers
             });
         }
 
-        public ActionResult Logout()
-        {
-            FormsAuthentication.SignOut();
-            return RedirectToRoute("home");
-        }
 
         [HttpPost]
         public ActionResult Login(AuthLogin form, string returnUrl)
         {
+            var user = Database.Session.Query<User>().FirstOrDefault(u => u.Username == form.Username);
+            if (user == null)
+                SimpleBlog.Models.User.FakeHash();
+
+            if (user == null || !user.CheckPassword(form.Password))
+                ModelState.AddModelError("Username", "Username or password is incorrect");
+
             if (!ModelState.IsValid)
                 return View(form);
-
+             
             FormsAuthentication.SetAuthCookie(form.Username, true);
 
             if (!string.IsNullOrWhiteSpace(returnUrl))
