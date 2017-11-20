108d5f00af989d44b55bbb4f0ec281678048b74a
jlife0113 <jlife0113@naver.com>
2017-11-20 15:12:26 +0900

    Front sidebar

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -25,6 +25,8 @@ namespace SimpleBlog
             routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
             routes.MapRoute("Logout", "logout", new { controller = "Auth", action = "Logout" }, namespaces);
             routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+
+            routes.MapRoute("Sidebar", "", new { controller = "Layout", action = "Sidebar" }, namespaces);
         }
     }
 }
